67e19d10ba467c6eb2a80ae2119ee436284b0fc8
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-12-16 10:57:04 -0500

    init commit

---
+++ SimpleCosumingJSONApp_MVVM_SwiftUI/SimpleCosumingJSONApp_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCosumingJSONApp_MVVM_SwiftUIApp.swift
+//  SimpleCosumingJSONApp_MVVM_SwiftUI
+//
+//  Created by Juan Pablo Lasprilla Correa on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCosumingJSONApp_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
